d42b6761a1137aef3b9b4d54216aab963c4d7206
amrit <amrit71082@gmail.com>
2025-05-08 23:15:43 +0530

    added features.p

--- ImageCaptionGenerator/main.py
+++ ImageCaptionGenerator/main.py
@@ -87,49 +87,67 @@ def save_descriptions(descriptions, filename):
 
 dataset_text =  "D:/documents/programming/projects/image captioning/ImageCaptionGenerator/Flickr8k_text"
 dataset_images = "D:/documents/programming/projects/image captioning/ImageCaptionGenerator/Flicker8k_Dataset"
+#
+#filename = dataset_text + "/Flickr8k.token.txt"
+#descriptions = all_img_captions(filename)
+#print("length of descriptions = ", len(descriptions))
+#
+#clean_descriptions = cleaning_text(descriptions)
+#vocabulary = text_vocabulary(clean_descriptions)
+#print("length of vocabulary = ",len(vocabulary))
+#
+#save_descriptions(clean_descriptions,"D:/documents/programming/projects/image captioning/ImageCaptionGenerator/descriptions.txt")
 
-filename = dataset_text + "/Flickr8k.token.txt"
-descriptions = all_img_captions(filename)
-print("length of descriptions = ", len(descriptions))
+#def download_with_retry(url,filename,max_retries = 3):
+#    for attempt in range(max_retries):
+#        try:
+#            return get_file(filename,url)
+#        except Exception as e:
+#            if attempt == max_retries -1:
+#                raise e
+#            print("Download failed")
+#            time.sleep(3)
+#
+#weights_url = "https://storage.googleapis.com/tensorflow/keras-applications/xception/xception_weights_tf_dim_ordering_tf_kernels_notop.h5"
+#weights_path = download_with_retry(weights_url,'xception_weights.h5')
+#model = Xception(include_top = False, pooling = "avg", weights = weights_path)
+#
+#def extract_features(directory):
+#    features = {}
+#    valid_images = [".jpg",".jpeg",".png"]
+#    for img in tqdm(os.listdir(directory)):
+#        ext = os.path.splitext(img)[1].lower()
+#        if ext not in valid_images:
+#            continue
+#        filename = directory + "/" + img
+#        image = Image.open(filename)
+#        image = image.resize((299,299))
+#        image = np.expand_dims(image,axis = 0)
+#        image = image / 127.5
+#        image = image - 1.0
+#        feature = model.predict(image)
+#        features[img] = feature
+#
+#    return features
+#
+#features = extract_features(dataset_images)
+#with open("D:/documents/programming/projects/image captioning/ImageCaptionGenerator/features.p", "wb") as file:
+#    dump(features, file)
 
-clean_descriptions = cleaning_text(descriptions)
-vocabulary = text_vocabulary(clean_descriptions)
-print("length of vocabulary = ",len(vocabulary))
+features = open("D:/documents/programming/projects/image captioning/ImageCaptionGenerator/features.p", "rb")
 
-save_descriptions(clean_descriptions,"D:/documents/programming/projects/image captioning/ImageCaptionGenerator/descriptions.txt")
+def load_photos(filename):
+    file = load_doc(filename)
+    photos = file.split("\n")[::-1]
+    photos_present = [photo for photo in photos if os.path.exists(os.path.join(dataset_images,photo))]
+    return photos_present
 
-def download_with_retry(url,filename,max_retries = 3):
-    for attempt in range(max_retries):
-        try:
-            return get_file(filename,url)
-        except Exception as e:
-            if attempt == max_retries -1:
-                raise e
-            print("Download failed")
-            time.sleep(3)
-
-weights_url = "https://storage.googleapis.com/tensorflow/keras-applications/xception/xception_weights_tf_dim_ordering_tf_kernels_notop.h5"
-weights_path = download_with_retry(weights_url,'xception_weights.h5')
-model = Xception(include_top = False, pooling = "avg", weights = weights_path)
-
-def extract_features(directory):
-    features = {}
-    valid_images = [".jpg",".jpeg",".png"]
-    for img in tqdm(os.listdir(directory)):
-        ext = os.path.splitext(img)[1].lower()
-        if ext not in valid_images:
+def load_clean_descriptions(filename,photos):
+    file = load_doc(filename)
+    descriptions = {}
+    for line in file.split("\n"):
+        words = line.split()
+        if len(words) < 1:
             continue
-        filename = directory + "/" + img
-        image = Image.open(filename)
-        image = image.resize((299,299))
-        image = np.expand_dims(image,axis = 0)
-        image = image / 127.5
 
-        feature = model.predict(image)
-        features[img] = feature
-
-    return features
-
-features = extract_features(dataset_images)
-with open("features.p", "wb") as file:
-    dump(features, file)
+        
